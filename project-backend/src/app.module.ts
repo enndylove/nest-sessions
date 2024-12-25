79b891b9e4d108134199f50aec5df877047a3a01
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-25 15:29:41 +0200

    update app module

--- project-backend/src/app.module.ts
+++ project-backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SessionController } from './session/session.controller';
+import { SessionService } from './session/session.service';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SessionModule],
+  controllers: [AppController, SessionController],
+  providers: [AppService, SessionService],
 })
 export class AppModule {}
